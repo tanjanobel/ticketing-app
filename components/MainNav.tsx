88659ae62aa402e18f8dfe347e883e54de27520b
Tanja Nobel <tanja@nobel.swiss>
2024-11-16 21:23:53 +0100

    Add navigation features

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,13 +1,12 @@
 import Link from 'next/link';
 import ToggleMode from "@/components/ToggleMode";
+import MainNavLinks from "@/components/MainNavLinks";
 
 const MainNav = () => {
     return (
         <div className="flex justify-between">
             <div className="flex items-center gap-2">
-                <Link href="/">Dashboard</Link>
-                <Link href="/tickets">Tickets</Link>
-                <Link href="/users">Users</Link>
+                <MainNavLinks />
             </div>
             <div className="flex items-center gap-2">
                 <Link href="/">Logout</Link>
