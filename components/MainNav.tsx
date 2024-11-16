ceaafd1b2863aea040ea0aaf4ae85f079fcc8217
Tanja Nobel <tanja@nobel.swiss>
2024-11-16 20:37:52 +0100

    Add theme and styles

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -2,10 +2,16 @@ import Link from 'next/link';
 
 const MainNav = () => {
     return (
-        <div>
-            <Link href="/">Dashboard</Link>
-            <Link href="/tickets">Tickets</Link>
-            <Link href="/users">Users</Link>
+        <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+                <Link href="/">Dashboard</Link>
+                <Link href="/tickets">Tickets</Link>
+                <Link href="/users">Users</Link>
+            </div>
+            <div className="flex items-center gap-2">
+                <Link href="/">Logout</Link>
+                <Link href="/">Dark</Link>
+            </div>
         </div>
     );
 };
