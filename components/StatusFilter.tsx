20529fa009dcb78543e513c1b3a385477f6ee3f4
Tanja Nobel <tanja@nobel.swiss>
2024-11-29 13:50:18 +0100

    Add status filter function

--- components/StatusFilter.tsx
+++ components/StatusFilter.tsx
@@ -25,7 +25,7 @@ const StatusFilter = () => {
                 router.push(`/tickets${query}`);
             }}
         >
-            <SelectTrigger>
+            <SelectTrigger className="w-[200px]">
                 <SelectValue placeholder="Filter by Status..." />
             </SelectTrigger>
             <SelectContent>
