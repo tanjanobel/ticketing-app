008c5f2a64433c1e3dc75f3cb35319f5b67bda52
Tanja Nobel <tanja@nobel.swiss>
2024-11-29 12:18:57 +0100

    Add status filter component

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,6 +3,7 @@ import DataTable from "@/app/tickets/DataTable";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import Pagination from "@/components/Pagination";
+import StatusFilter from "@/components/StatusFilter";
 
 interface SearchParams {
     page: string,
@@ -25,6 +26,7 @@ const Tickets = async ({searchParams}: { searchParams: SearchParams }) => {
             >
                 New Ticket
             </Link>
+            <StatusFilter />
             <DataTable tickets={tickets} />
             <Pagination
                 itemCount={ticketCount}
