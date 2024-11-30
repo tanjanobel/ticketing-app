2a9baf04cecc60cca56b81f74985822134397c0c
Tanja Nobel <tanja@nobel.swiss>
2024-11-30 11:24:11 +0100

    Update user api

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,52 @@
+import prisma from "@/prisma/db";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+import { userSchema } from "@/validationSchemas/users";
+
+interface Props {
+    params: { id: string };
+}
+
+export async function PATCH(request: NextRequest, {params}: Props) {
+    const body = await request.json();
+    const validation = userSchema.safeParse(body);
+
+    if (!validation.success) {
+        return NextResponse.json(validation.error.format(), {status: 400});
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {id: parseInt(params.id)},
+    });
+
+    if (!user) {
+        return NextResponse.json({error: "User Not Found"}, {status: 404});
+    }
+
+    if (body?.password) {
+        const hashPassword = await bcrypt.hash(body.password, 10);
+        body.password = hashPassword;
+    }
+
+    if (user.username === body.username) {
+        const duplicateUsername = await prisma.user.findUnique({
+            where: {username: body.username},
+        });
+
+        if (duplicateUsername) {
+            return NextResponse.json(
+                {message: "Duplicate username"},
+                {status: 409}
+            );
+        }
+    }
+
+    const updatedUser = await prisma.user.update({
+        where: {id: user.id},
+        data: {
+            ...body
+        },
+    });
+
+    return NextResponse.json(updatedUser, {status: 200});
+}
