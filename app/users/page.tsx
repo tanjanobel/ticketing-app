5ac0d203306af18682ba0f69d1b93cf60f567f8d
Tanja Nobel <tanja@nobel.swiss>
2024-12-06 09:43:37 +0100

    Add user table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,14 @@
 import UserForm from "@/components/UserForm";
+import DataTableSimple from "@/app/users/data-table-simple";
+import prisma from "@/prisma/db";
 
-const Users = () => {
+const Users =  async () => {
+
+    const user = await prisma.user.findMany()
     return (
         <div>
             <UserForm />
+            <DataTableSimple users={user} />
         </div>
     );
 };
